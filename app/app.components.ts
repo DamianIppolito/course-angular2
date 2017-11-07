42915adc08dad8d615f03ecf04c254e0dc89966b
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-11-07 11:54:06 +0100

    Seccion 3 - 20 - Directiva NgIf, directiva NgClass y evento Click

--- app/app.components.ts
+++ app/app.components.ts
@@ -10,13 +10,19 @@ import {Pelicula} from './model/pelicula';
 export class AppComponent{
   public titulo:string = "Películas con Angular2";
   public pelicula:Pelicula;
+  public mostrarDatos:boolean;
 
   constructor(){
+    this.mostrarDatos = false;
     this.pelicula = new Pelicula(1, "Batman vs Superman", "Zack Snider", 2016);
     this.debug();
   }
 
   debug(){
     console.log(this.pelicula);
+  }
+
+  onShowHide(value){
+    this.mostrarDatos = value;
   }
 }
