1f9265c69e4dd1dae7135748896d0bf47ebb8cd8
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-11-07 13:59:52 +0100

    Seccion 4 - 26 - Crear y usar un servicio

--- app/components/peliculas-list.component.ts
+++ app/components/peliculas-list.component.ts
@@ -1,9 +1,11 @@
 import  {Component} from "angular2/core";
 import {Pelicula} from '../model/pelicula';
+import { PeliculasService } from '../services/peliculas.service';
 
 @Component({
   selector: "peliculas-list",
-  templateUrl: "app/view/peliculas-list.html"
+  templateUrl: "app/view/peliculas-list.html",
+  providers: [PeliculasService]
 })
 
 export class PeliculasListComponent{
@@ -11,19 +13,13 @@ export class PeliculasListComponent{
   public peliculaElegida:Pelicula;
   public mostrarDatos:boolean;
   public peliculas:Array<Pelicula>;
+  public datoServicio;
 
-  constructor(){
+  constructor(private _peliculasService: PeliculasService){
     this.mostrarDatos = false;
-    this.peliculas = [
-      new Pelicula(1, "Batman vs Superman", "Zack Snider", 2016),
-      new Pelicula(2, "La verdad duele", "Will Smith", 2016),
-      new Pelicula(3, "El señor de los anillos", "Peter Jackson",null),
-      new Pelicula(4, "Una historia real", "El de supersalidos", 2015),
-      new Pelicula(5, "Don Jon", "joseph gordon levitt", 2014)
-    ];
+    this.peliculas = this._peliculasService.getPeliculas();
     this.pelicula = this.peliculas[0];
     this.peliculaElegida = this.peliculas[0];
-    this.debug();
   }
 
   debug(titulo = null){
---
+++ app/services/mock.peliculas.ts
@@ -0,0 +1,10 @@
+import { Pelicula } from '../model/pelicula';
+
+export const PELICULAS:Array<Pelicula> = [
+  new Pelicula(1, "Batman vs Superman", "Zack Snider", 2016),
+  new Pelicula(2, "La verdad duele", "Will Smith", 2016),
+  new Pelicula(3, "El señor de los anillos", "Peter Jackson",null),
+  new Pelicula(4, "Una historia real", "El de supersalidos", 2015),
+  new Pelicula(5, "Don Jon", "joseph gordon levitt", 2014),
+  new Pelicula(6, "Dos tontos muy tontos", "Desconocido", 2001)
+];
